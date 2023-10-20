8e1db26d3ed817192595900554b47905967eadf1
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-10-21 00:42:00 +0530

    Updated server

--- server/main.js
+++ server/main.js
@@ -4,12 +4,7 @@ const fs = require("fs");
 const port = 6969;
 const { Web3 } = require('web3');
 require('dotenv').config();
-const contractAddressArray = ["0x7512fBd4A1186Dc1F465D3F60a9a323c72d406CA", "", "", "", "", "", "", "", ""]; // change to where the contract for each token is deployed
-// const blockParameter = 'latest';
-// const gasLimit = 200000; // Set your desired gas limit
-// // If you are using an EIP-1559 transaction, also set the following parameters:
-// const maxPriorityFeePerGas = '1000000000'; // Replace with your desired maxPriorityFeePerGas
-// const maxFeePerGas = '2000000000'; // Replace with your desired maxFeePerGas
+const contractAddressArray = ["0x7512fBd4A1186Dc1F465D3F60a9a323c72d406CA", "", "", "", "", "", "", "", ""];
 const TOKENS = ["ARB", "BNB", "BTC", "DOGE", "ETH", "OP", "PEPE", "PUSH", "USDT"]
 let contractABIArray = ["", "", "", "", "", "", "", "", ""];
 let DeployerArray = ["0x7DB89eEadF8a526e7EDaedCF3DdBd0452B7F4c8b","","","","","","","",""];
@@ -30,16 +25,6 @@ for (let i = 0; i < TOKENS.length; i++) {
         }
     });
 }
-
-// const { addClose, getClose } = require("./Params/CLOSE/close");
-// const { setGithubCommits, getGithubCommits } = require("./Params/GITHUB/github");
-// const { addHigh, getHigh } = require("./Params/HIGH/high");
-// const { addLow, getLow } = require("./Params/LOW/low");
-// const { addMarketCap, getMarketCap } = require("./Params/MARKET-CAP/market-cap");
-// const { setMarkets, getMarkets } = require("./Params/MARKETS/markets");
-// const { addOpen, getOpen } = require("./Params/OPEN/open");
-// const { setTwitterFollowers, getTwitterFollowers } = require("./Params/TWITTER/twitter");
-
 
 app.use(express.json()); // to get the data in json format
 app.use(express.urlencoded({ extended: true })); // to get the form data
@@ -76,24 +61,3 @@ app.get("/tokens/:token", async (req, res) => {
 app.listen(port, () => {
     console.log(`App listening at port ${port}`);
 });
-// Open.forEach((element, index) => {
-//     Open[index] = element / BigInt(1e10);
-// });
-
-// Close.forEach((element, index) => {
-//     Close[index] = element / BigInt(1e10);
-// });
-
-// High.forEach((element, index) => {
-//     High[index] = element / BigInt(1e10);
-// });
-
-// Low.forEach((element, index) => {
-//     Low[index] = element / BigInt(1e10);
-// });
-
-// MarketCap.forEach((element, index) => {
-//     MarketCap[index] = element / BigInt(1e10);
-// });
-
-// token: req.params.token, twitter_followers: (Twitter_Followers) / BigInt(1e10), Github_Commits: (Github_Commits) / BigInt(1e10), Markets: (Markets) / BigInt(1e10), Open: Open[Open.length - 1], Close: Close[Close.length - 1], High: High[High.length - 1], Low: Low[Low.length - 1], MarketCap: MarketCap[MarketCap.length - 1] 

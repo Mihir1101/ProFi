d995a7bf9247775a545bc49f79fdf2c8b2210bb1
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-10-22 11:31:19 +0530

    compared deployer and connected address

--- server/main.js
+++ server/main.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const port = 6969;
 const { Web3 } = require('web3');
 require('dotenv').config();
-const contractAddressArray = ["", "", "", "", "", "", "", "", ""];
 const SepoliaContractAddressArray = ["ARB", "BNB", "0xEf68a0a81E65FCcC8e8E934B48CE7cc8d13B6055", "0x48424D7C9A7C44EC0ad3C73d983E20E248aBD4d3", "0xd0eF0811Cb353a8d5c09AAbE7acf1CD0776d09CE", "OP", "PEPE", "PUSH", "USDT"];
 const FileCoinContractAddressArray = ["ARB", "BNB", "0xE6dD79C6D7C1B959E6DD87838ed8cA571c632172", "0x7bb3aF97694802B4665cf74079376d2167Bf03d5", "0xD043f19c25a83903788C95CE39a8Be0064896a4e", "OP", "PEPE", "PUSH", "USDT"];
 const MantleContractAddressArray = ["ARB", "BNB", "0x7d1bB6B83EdCB9378d3C4647E218BCB1Dea19CCE", "0x79bd4b4c662810f049171FDEb3563826b12Df65B", "0xE6dD79C6D7C1B959E6DD87838ed8cA571c632172", "OP", "PEPE", "PUSH", "USDT"];
@@ -48,7 +47,7 @@ app.get("/tokens/:token", async (req, res) => {
         let Deployer;
         for (let i = 0; i < TOKENS.length; i++) {
             if (TOKENS[i] == req.params.token) {
-                contractAddress = contractAddressArray[i];
+                contractAddress = FileCoinContractAddressArray[i];
                 contractABI = contractABIArray[i];
                 Deployer = DeployerArray[i];
                 break;
